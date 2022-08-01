1037633795a5e320e24a85e055feb2d5b976a4df
Mansi Patil <108403813+MansiPatil33@users.noreply.github.com>
2022-08-01 15:37:04 +0530

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,9 @@
+function toggle(){
+    var x = document.getElementById('a')
+    if(x.innerHTML == 'Welcome to JavaScript'){
+    x.innerHTML = 'Friday it is !!!'
+    }else{
+        x.innerHTML = 'Welcome to JavaScript'
+    }
+
+}
